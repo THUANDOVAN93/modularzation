8c6f58c012e68e1d44e1a0b43c6e9c635acfc281
THUAN DO VAN <thuandovan93@gmail.com>
2025-12-23 21:58:09 +0700

    Automatically Loading Views and Components

---
+++ modules/Order/Views/checkout/index.blade.php
@@ -0,0 +1,6 @@
+<div>
+    You're on the checkout page
+
+    <x-order::alert message="Something is working"></x-order::alert>
+    <x-order::order-line product="Test product"></x-order::order-line>
+</div>

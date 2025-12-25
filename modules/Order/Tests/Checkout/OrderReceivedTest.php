f58c0b690c2f7fcf4c59824054fafe634688944c
THUAN DO VAN <thuandovan93@gmail.com>
2025-12-25 19:46:12 +0700

    Email testing

---
+++ modules/Order/Tests/Checkout/OrderReceivedTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Checkout;
+
+use Modules\Order\Checkout\OrderReceived;
+use Modules\Order\Contracts\OrderDto;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class OrderReceivedTest extends TestCase
+{
+    #[Test]
+    public function it_renders_the_mailable(): void
+    {
+        $orderDto = new OrderDto(
+          id: 1,
+          totalInCents: 99_00,
+          localizedTotal: '$99.00',
+          url: route('orders.show', 1),
+          lines: []
+        );
+
+        $orderReceived = new OrderReceived($orderDto);
+        $this->assertIsString($orderReceived->render());
+    }
+}
